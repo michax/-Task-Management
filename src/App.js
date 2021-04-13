73351a206aba60e2feaebfdbffee2d72acb3b2ff
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-04-13 13:46:53 +0200

    added component responsble for counting down , added function responsible for handeling button add , got some issues - input with time dont get value and count down start to count after open app

--- src/App.js
+++ src/App.js
@@ -5,19 +5,56 @@ import React from 'react';
 
 
 
+class CountDownTimer extends React.Component {
+
+  intervalId=null;
+
+  constructor(props){
+    super(props)
+
+    this.state={
+      counter: 5, // jak pobrac wartość z this state counter do mojego inputa gdzie wpisuje mój Czas do zadania ?
+    }
+  }
+
+  componentDidMount(){
+    console.log('Uruchumienie licznika', {nazwa:this.props.title})
+
+    this.intervalId=setInterval(()=>{
+
+      console.log('Aktualizuj licznik')
+     
+
+      this.setState((prevState)=>{
+
+        if (prevState.counter === 1 ) {
+          clearInterval(this.intervalId)
+          console.log('Ding!');
+        }
+        return{
+          counter: prevState.counter - 1,
+        }
+      })
+    },1000)
+
+  }
 
 
+  componentWillUnmount(){
+      clearInterval(this.intervalId)
+  }
+  
 
+  render(){
+    return(
+      <div>
+        <p>Name of Task: {this.props.title} My time: {this.props.counter}</p> 
+        <p>Time Remaining: {this.state.counter}</p> 
+      </div>
+    )
+  }
 
-
-
-
-
-
-
-
-
-
+}
 
 class App extends React.Component{
   constructor(props){
@@ -37,6 +74,21 @@ class App extends React.Component{
 
   }
 
+  handlerAddTask = () => {
+    const inputTaskText= this.inputFieldText.current.value;
+    const inputTimeTask=this.inputFieldTime.current.value;
+    console.log(inputTaskText,inputTimeTask)
+
+    this.setState((prevState)=>{
+      const newListTasks = [...prevState.listTasks, {
+        name:inputTaskText,
+        time:inputTimeTask,
+      }]
+      return {
+        listTasks: newListTasks,
+      };
+    });
+  };
 
 
 
@@ -48,7 +100,16 @@ class App extends React.Component{
             <input ref={this.inputFieldText} type="text" placeholder="your task for today"></input>
             <input ref={this.inputFieldTime} type="number" placeholder="for how long"></input>
         </form>
-        <button>Add task </button>
+        <ul>
+            {
+              this.state.listTasks.map(elem=>{
+                return (
+                  <CountDownTimer title={elem.name} counter={elem.time}/>
+                )
+              })
+            }
+          </ul>
+          <button onClick={this.handlerAddTask}>Add task</button>
       </div>
     )
   }
