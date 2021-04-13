138465066366dc2e84914e06ce058c6d01008be6
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-04-13 13:27:58 +0200

    inital project , added main markup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return(
+      <div className='App'>
+        <h1>Timer`s to Do Lists</h1>
+        <form>
+          <input type="text" placeholder="your task for today"></input>
+          <input type="number" placeholder="for how long"></input>
+        </form>
+        <button>Add task </button>
+      </div>
+    )
+  }
+
 }
-
 export default App;
