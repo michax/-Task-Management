cb53abd2af9ba3fb6c43b0b4263351a501a5921a
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-04-13 13:37:10 +0200

    added access to interact the DOM thanks react refs , we can have value from input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
 
@@ -22,15 +22,31 @@ import React from 'react';
 class App extends React.Component{
   constructor(props){
     super(props)
+
+    this.inputFieldText= React.createRef();
+    this.inputFieldTime= React.createRef();
+
+    this.state = {
+      listTasks: [
+        {
+          // name: 'Nauka Metod tablic w Js',
+          // time: 1000,
+        }
+      ]
+    }
+
   }
+
+
+
 
   render(){
     return(
       <div className='App'>
         <h1>Timer`s to Do Lists</h1>
         <form>
-          <input type="text" placeholder="your task for today"></input>
-          <input type="number" placeholder="for how long"></input>
+            <input ref={this.inputFieldText} type="text" placeholder="your task for today"></input>
+            <input ref={this.inputFieldTime} type="number" placeholder="for how long"></input>
         </form>
         <button>Add task </button>
       </div>
